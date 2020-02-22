3f4b99140d0dd25fed04bf918abb7d591ab8c5ec
sunho <sunho@sunhos-MacBook-Pro.local>
2020-02-23 07:50:57 +0900

    Add pics

--- deploy.js
+++ deploy.js
@@ -1,3 +1,14 @@
 var ghpages = require('gh-pages');
-
-ghpages.publish('_site', {branch:'master'}, function(err) {console.log(err)});
+const { exec } = require("child_process");
+exec("JEKYLL_ENV=production bundle exec jekyll build", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    ghpages.publish('_site', {branch:'master'}, function(err) {console.log(err)});
+});
