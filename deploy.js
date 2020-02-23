46487164e35d8f8920696e84996dd45c054a780b
sunho <sunho@sunhos-MacBook-Pro.local>
2020-02-23 17:56:57 +0900

    Updates

--- deploy.js
+++ deploy.js
@@ -1,5 +1,8 @@
 var ghpages = require('gh-pages');
 const { exec } = require("child_process");
+var fs = require('fs');
+
+
 exec("JEKYLL_ENV=production bundle exec jekyll build", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
@@ -10,5 +13,6 @@ exec("JEKYLL_ENV=production bundle exec jekyll build", (error, stdout, stderr) =
         return;
     }
     console.log(`stdout: ${stdout}`);
+    fs.writeFile('_site/CNAME', "sunho.kim", function(err) {});
     ghpages.publish('_site', {branch:'master'}, function(err) {console.log(err)});
 });
