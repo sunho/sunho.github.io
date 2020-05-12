c1960cbf82eb1c438a676a8ad83fc2ef7a100b8b
Sunho Kim <ksunhokim123@naver.com>
2020-05-12 11:25:11 +0900

    Convert to Hugo

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 var ghpages = require('gh-pages');
 const { exec } = require("child_process");
-exec("JEKYLL_ENV=production bundle exec jekyll build", (error, stdout, stderr) => {
+exec("hugo", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -10,5 +10,5 @@ exec("JEKYLL_ENV=production bundle exec jekyll build", (error, stdout, stderr) =
         return;
     }
     console.log(`stdout: ${stdout}`);
-    ghpages.publish('_site', {branch:'master'}, function(err) {console.log(err)});
+    ghpages.publish('public', {branch:'master'}, function(err) {console.log(err)});
 });
